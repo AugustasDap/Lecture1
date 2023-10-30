cd629ac1c70587687fa67d38ba774d90a35b5180
Augustas <Augustas@LAPTOP-CMISBDB7>
2023-10-30 21:20:34 +0200

    Add project files.

---
+++ First/Program.cs
@@ -0,0 +1,68 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace FirstProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("1:");
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" /    \\");
+            Console.WriteLine("/______\\");
+            Console.WriteLine("1.1:");
+            Console.WriteLine("\n   /\\\n  /  \\\n /    \\\n/______\\\n");
+            Console.WriteLine("2:");
+            Console.WriteLine("     *****");
+            Console.WriteLine("   **     **");
+            Console.WriteLine(" **         **");
+            Console.WriteLine("**   Hello   **");
+            Console.WriteLine("**    from   **");
+            Console.WriteLine(" **   C#    **");
+            Console.WriteLine("   **     **");
+            Console.WriteLine("     *****");
+            Console.WriteLine("3:");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            {
+                Console.WriteLine("   /\\");
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            {
+                Console.WriteLine("  /  \\");
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            {
+                Console.WriteLine(" /    \\");
+            }
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            {
+                Console.WriteLine("/______\\");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("4:");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine(" / \\     / \\");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("/   \\   /   \\");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("\\    \\ /    /");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(" \\         /");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("  \\       /");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("   \\     /");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("    \\   /");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("     \\ /");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("      V");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+    }
+}
+
